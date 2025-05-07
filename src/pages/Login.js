eb3ee851f472ae2108483364249dbf5d8ae2f6db
abhi-7-7 <aarsh4344@gmail.com>
2025-05-07 12:44:05 +0530

    Auth css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Auth.css';
+
 const Login = () => {
   const router = useNavigate();
   const [form, setForm] = useState({
